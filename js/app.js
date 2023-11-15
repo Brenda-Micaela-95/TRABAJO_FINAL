bf713f95f165e02df1febbb8ba8938fd0f7bc29c
Brenda Micaela <brenda.micaela.9521@gmail.com>
2023-11-15 13:26:19 -0300

    modificaciones

---
+++ js/app.js
@@ -0,0 +1,39 @@
+var imagenes = document.querySelectorAll('.contenedor img');
+        var imagenActual = 0;
+
+        setInterval(function() {
+            imagenes[imagenActual].classList.remove('activar');
+
+            imagenActual++;
+            if (imagenActual >= imagenes.length) {
+                imagenActual = 0;
+            }
+
+            imagenes[imagenActual].classList.add('activar');
+        }, 5000);
+
+
+        var prevButton = document.querySelector('.prev');
+        var nextButton = document.querySelector('.next');
+
+        prevButton.addEventListener('click', function() {
+            imagenes[imagenActual].classList.remove('activar');
+
+            imagenActual--;
+            if (imagenActual < 0) {
+                imagenActual = imagenes.length - 1;
+            }
+
+            imagenes[imagenActual].classList.add('activar');
+        });
+
+        nextButton.addEventListener('click', function() {
+            imagenes[imagenActual].classList.remove('activar');
+
+            imagenActual++;
+            if (imagenActual >= imagenes.length) {
+                imagenActual = 0;
+            }
+
+            imagenes[imagenActual].classList.add('activar');
+        });
